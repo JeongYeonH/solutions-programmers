763b9a515f31ba8fd0909a929227156e52852090
Jeong Yeon Hwang <kukuro1995@naver.com>
2024-01-03 10:22:07 +0900

    41번쨰

---
+++ test.js
@@ -0,0 +1,7 @@
+function solution(s) {
+    const words = s.split(' ');
+    return words.map(word => word.split('').map((alphabet, index)=> index%2==0 ? alphabet.toUpperCase(): alphabet.toLowerCase()).join('')).join(' ');
+}
+var s = "those has been";
+console.log(solution(s));
+
