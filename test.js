902ce573a90d7553954ea6559205aaa19f5fdcc6
Jeong Yeon Hwang <kukuro1995@naver.com>
2024-01-05 09:48:57 +0900

    두 번째 문제

--- test.js
+++ test.js
@@ -1,7 +1,20 @@
-function solution(s) {
-    const words = s.split(' ');
-    return words.map(word => word.split('').map((alphabet, index)=> index%2==0 ? alphabet.toUpperCase(): alphabet.toLowerCase()).join('')).join(' ');
+function solution(t, p) {
+    var count = 0;
+    var num = 0;
+    for(i=0; i<=t.length-p.length; i++){
+        for(j=i; j<i+p.length; j++){
+            num +=t[j];
+        }
+        if(+num<= +p){
+            console.log(num);
+            count++;
+        }
+        num = 0;
+
+    }
+    return count;
 }
-var s = "those has been";
-console.log(solution(s));
+var t = "10203";
+var p = "15";
+console.log(solution(t, p));
 
