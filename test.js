58652d7cbadbfa0af3acccacc38da6d5e5a22142
Jeong Yeon Hwang <kukuro1995@naver.com>
2024-01-06 01:55:06 +0900

    47

--- test.js
+++ test.js
@@ -1,38 +1,9 @@
-function solution(s) {
-    count = 0;
-    var string = ''
-    while(s.length > 0){
-        for(num in numbers){
-            var position = s.indexOf(num);
-            if(position!=-1 && position == 0){                           
-                s = s.slice(position+num.length);
-                string +=numbers[num];
-            }else if(/^\d+$/.test(s[0])){
-                string +=s[0];
-                s = s.slice(1);
-            }
-        }       
-    }
-    return +string;
+function solution(strings, n) {
+    strings = strings.map(i => i[n]+i).sort();
+    strings = strings.map(i => i.substring(1));
+    return strings;
 }
 
-var numbers = {
-    "zero" : "0",
-    "one" : "1",
-    "two" : "2",
-    "three" : "3",
-    "four" : "4",
-    "five" : "5",
-    "six" : "6",
-    "seven" : "7",
-    "eight" : "8",
-    "nine" : "9",
-}
-
-function method(s){
-    console.log(s.slice(0,3));
-}
-
-var s = "zeronine3fivezerozero3";
-console.log(solution(s));
-
+var strings = ["sun", "bed", "car"]
+var n = 1;
+console.log(solution(strings, n));
