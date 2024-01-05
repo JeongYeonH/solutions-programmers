54d26d768a204f80d95406d4542ce74ca8b1a0f7
Jeong Yeon Hwang <kukuro1995@naver.com>
2024-01-05 22:31:59 +0900

    46

--- test.js
+++ test.js
@@ -1,15 +1,38 @@
-function solution(s, n) {
-    return s.split('').map(char=> {if(char.match(/[a-zA-Z]/)){
-        let num = char.charCodeAt();
-        let Big = char === char.toUpperCase();
-        num = (Big ? 65 : 97) + (num - (Big ? 65 : 97) + n + 26) % 26;
-        return String.fromCharCode(num);
-    }else{
-        return char;
-    }}).join('');
-    
+function solution(s) {
+    count = 0;
+    var string = ''
+    while(s.length > 0){
+        for(num in numbers){
+            var position = s.indexOf(num);
+            if(position!=-1 && position == 0){                           
+                s = s.slice(position+num.length);
+                string +=numbers[num];
+            }else if(/^\d+$/.test(s[0])){
+                string +=s[0];
+                s = s.slice(1);
+            }
+        }       
+    }
+    return +string;
 }
-var s = "a B z";
-var n = 4;
-console.log(solution(s, n));
+
+var numbers = {
+    "zero" : "0",
+    "one" : "1",
+    "two" : "2",
+    "three" : "3",
+    "four" : "4",
+    "five" : "5",
+    "six" : "6",
+    "seven" : "7",
+    "eight" : "8",
+    "nine" : "9",
+}
+
+function method(s){
+    console.log(s.slice(0,3));
+}
+
+var s = "zeronine3fivezerozero3";
+console.log(solution(s));
 
