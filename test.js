603564539aab04aa3bee53ddfcf26a3d16c05a5e
Jeong Yeon Hwang <kukuro1995@naver.com>
2024-01-07 22:25:40 +0900

    49

--- test.js
+++ test.js
@@ -1,15 +1,15 @@
-function solution(array, commands) {
-    var newArr = [];
-    for(i=0; i< commands.length; i++){
-        var start = commands[i][0];
-        var end = commands[i][1];
-        var pick = commands[i][2]-1;
-        var dedicatedNum = array.slice(start-1, end).sort((a,b)=> a-b)[pick];
-        newArr.push(dedicatedNum);           
+function solution(numbers) {
+    var newArr = []
+    for(i=0; i<numbers.length; i++){
+        for(j=i+1; j<numbers.length; j++){
+            var add = numbers[i]+numbers[j];
+            if(!newArr.includes(add)){
+                newArr.push(add);
+            }
+        }
     }
-    return newArr;
+    return newArr.sort((a,b)=> a-b);
 }
 
-var array = [10, 2];
-var commands = [[1, 2, 1]];
-console.log(solution(array, commands));
+var numbers = [2,1,3,4,1];
+console.log(solution(numbers));
