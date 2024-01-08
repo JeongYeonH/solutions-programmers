f7e1332c2bb945c07d9887e9e5ff7eebcb8aab0d
Jeong Yeon Hwang <kukuro1995@naver.com>
2024-01-08 09:41:43 +0900

    50

--- test.js
+++ test.js
@@ -1,15 +1,22 @@
-function solution(numbers) {
+function solution(s) {
     var newArr = []
-    for(i=0; i<numbers.length; i++){
-        for(j=i+1; j<numbers.length; j++){
-            var add = numbers[i]+numbers[j];
-            if(!newArr.includes(add)){
-                newArr.push(add);
+    // 먼저 주어진 문자열의 각 문자를 순서대로 훑어봅니다.
+    for(i=0; i<s.length; i++){
+        // 그리고 각 문자의 위치에서 처음 문자까지 거꾸로 훓어봅니다.
+        // 같은 위치에 있는 건 무조건 동일하므로 제외합니다.
+        for(j=i-1; j>= 0; j--){
+            // 이들 중 일치하는 부위를 찾습니다.
+            var count = 0;
+            if(s[j]===s[i]){
+                console.log(`${i-j}번째에서 일치`);
+                // 맨 처음 일치하면 다음 일치하는 부위는 찾을 필요가 없으므로 break합니다.
+                break;
             }
         }
     }
-    return newArr.sort((a,b)=> a-b);
+    return newArr;
 }
 
-var numbers = [2,1,3,4,1];
-console.log(solution(numbers));
+// 아직 다 풀지 못했습니다.
+var s = "banana";
+console.log(solution(s));
