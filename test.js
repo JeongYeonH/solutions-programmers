84f5d78f4c5abf1b6da5b462c9c0fd95a6c83066
Jeong Yeon Hwang <kukuro1995@naver.com>
2024-01-09 17:55:06 +0900

    51

--- test.js
+++ test.js
@@ -1,5 +1,11 @@
-function solution(s) {
-    const words = s.split(' ');
-    return words.map(word => word.split('').map((alphabet, index)=> index%2==0 ? alphabet.toUpperCase(): alphabet.toLowerCase()).join('')).join(' ');
+function solution(food) {
+    var answer = '';
+    for(i=1; i<food.length; i++){
+        var howMany = Math.floor(food[i]/2);
+        answer += (i+'').repeat(howMany);
+    }
+    return answer + '0' + answer.split('').reverse().join('');
 }
 
+var food = [1, 3, 4, 6];
+console.log(solution(food));
