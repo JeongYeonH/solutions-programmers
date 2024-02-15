8599e1b0ae3ca20af3ca8414c9c9c33117dec819
Jeong Yeon Hwang <kukuro1995@naver.com>
2024-02-15 14:44:09 +0900

    68

--- 68.js
+++ 68.js
@@ -1,6 +1,27 @@
 function solution(ingredient) {
-    var answer = 0;
-    return answer;
+  var i = 0;
+  var hambuger = 0;
+  var restart = true;
+  while (restart) {
+    
+    if (
+      ingredient[i] === 1 &&
+      ingredient[i + 1] === 2 &&
+      ingredient[i + 2] === 3 &&
+      ingredient[i + 3] === 1
+    ) {
+      hambuger++;
+      ingredient.splice(i, 4);
+      if(i !==0){
+        i = i-3;
+      }
+    } else if( i > ingredient.length-3) {
+      restart = false;
+    }else{
+        i++;
+    }
+  }
+  return hambuger;
 }
 
 var ingredient = [2, 1, 1, 2, 3, 1, 2, 3, 1];
---
+++ 69.js
@@ -0,0 +1,9 @@
+function solution(survey, choices) {
+    var answer = '';
+    return answer;
+}
+
+var survey = ["AN", "CF", "MJ", "RT", "NA"];
+var choices = [5, 3, 2, 7, 5];
+
+console.log(solution(survey, choices));
